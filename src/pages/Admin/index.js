db0163b87dfd8175faadafef4a79328246c22245
INDRA\lnunes <lnunes@minsait.com>
2023-03-27 14:32:12 -0300

    tela inicial apos login

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,43 @@
+import { useState } from 'react';
 import './admin.css';
+import { auth } from '../../firebaseConnection';
+import { signOut } from 'firebase/auth';
 
 export default function Admin(){
+
+    const [tarefaInput, setTarefaInput] = useState('');
+
+    function handleRegistrarTarefa(e){
+        e.preventDefault();
+        alert("Clique");
+    }
+
+    async function handleLogout(){
+        await signOut(auth);
+    }
+
     return(
-        <div>
+        <div className='admin-container'>
             <h1>Você está dentro</h1>
+
+            <form className="form" onSubmit={handleRegistrarTarefa}>
+                <textarea 
+                    placeholder='Digite sua tarefa...'
+                    value={tarefaInput}
+                    onChange={(e)=> {setTarefaInput= e.target.value}}
+                />
+                <button className='btn-register' type="submit">Registrar tarefa</button>
+            </form>
+
+            <article className="list">
+                <p>Fazer comprar hoje</p>
+                <div>
+                    <button>Editar</button>
+                    <button className="btn-delete">Concluir</button>
+                </div>
+            </article>
+
+            <button className="btn_logout" onClick={handleLogout}>Sair</button>
         </div>
     );
 }
